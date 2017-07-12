3df3b71d08524de986b3897ad053b6a51393a462
Roman Shuvalov <rshuvalov@abtronics.ru>
2017-07-12 16:09:50 +0300

    Update screenshot usage with upscale and downscale

--- src/main.cpp
+++ src/main.cpp
@@ -43,16 +43,31 @@ int main() {
     );
 
     X11Screenshot screenshot = X11Screenshot(image);
-    cout << "x11 init" << endl;
-    if (screenshot.save_to_jpeg("test.jpg", 30))
-        cout << "saved jpeg" << endl;
-    if (screenshot.save_to_png("test.png"))
-        cout << "saved png" << endl;
+    if (screenshot.save_to_jpeg("original.jpg", 30))
+        cout << "saved original jpeg" << endl;
+    if (screenshot.save_to_png("original.png"))
+        cout << "saved original png" << endl;
     X11Screenshot thumbnail = X11Screenshot(image, 1920, 540);
-    if (thumbnail.save_to_jpeg("thumb_test.jpg", 30))
-        cout << "saved thumbnail jpeg" << endl;
-    if (thumbnail.save_to_png("thumb_test.png"))
-        cout << "saved thumbnail png" << endl;
+    if (thumbnail.save_to_jpeg("linear_downscale.jpg", 50))
+        cout << "saved linear downscaled jpeg" << endl;
+    if (thumbnail.save_to_png("linear_downscale.png"))
+        cout << "saved linear downscaled png" << endl;
+    X11Screenshot upscale = X11Screenshot(image, 7680, 2160);
+    if (upscale.save_to_jpeg("linear_upscale.jpg", 30))
+        cout << "saved linear upscale jpeg" << endl;
+    if (upscale.save_to_png("linear_upscale.png"))
+        cout << "saved linear upscale png" << endl;
+    X11Screenshot bl_thumbnail = X11Screenshot(image, 2540, 720, "bilinear");
+    if (bl_thumbnail.save_to_jpeg("bilinear_downscale.jpg", 30))
+        cout << "saved bilinear downscale jpeg" << endl;
+    if (bl_thumbnail.save_to_png("bilinear_downscale.png"))
+        cout << "saved bilinear downscale png" << endl;
+    X11Screenshot b_upscale = X11Screenshot(image, 7680, 2160, "bilinear");
+    if (b_upscale.save_to_jpeg("bilinear_upscale.jpg", 30))
+        cout << "saved bilinear upscale jpeg" << endl;
+    if (b_upscale.save_to_png("b_upscale_test.png"))
+        cout << "saved bilinear upscale png" << endl;
+    
     XDestroyImage(image);
     XCloseDisplay(display);
     return 0;
